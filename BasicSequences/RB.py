7c47d6d1f5536865336a2783f91a0fe7444a0ef5
Colm Ryan <cryan@bbn.com>
2013-02-08 10:35:32 -0500

    Atomic Cliffords and RB_AC

--- BasicSequences/RB.py
+++ BasicSequences/RB.py
@@ -5,7 +5,7 @@ from csv import reader
 def SingleQubitRB(qubit, seqFile, showPlot=False):
 	"""
 
-	Single qubit randomized benchmarking. 
+	Single qubit randomized benchmarking using 90 and 180 generators. 
 
 	Parameters
 	----------
@@ -28,11 +28,11 @@ def SingleQubitRB(qubit, seqFile, showPlot=False):
 		fileReader = reader(FID, delimiter='\t')
 		seqs = []
 		for pulseSeqStr in fileReader:
-			tmpSeq = []
-			for tmpPulseStr in pulseSeqStr:
-				tmpSeq.append(pulseLib[tmpPulseStr])
-			tmpSeq.append(measBlock)
-			seqs.append(tmpSeq)
+			seq = []
+			for pulseStr in pulseSeqStr:
+				seq.append(pulseLib[pulseStr])
+			seq.append(measBlock)
+			seqs.append(seq)
 
 	#Tack on the calibration scalings
 	seqs += [[Id(qubit), measBlock], [Id(qubit), measBlock], [X(qubit), measBlock], [X(qubit), measBlock]]
@@ -44,3 +44,56 @@ def SingleQubitRB(qubit, seqFile, showPlot=False):
 	if showPlot:
 		plotWin = plot_pulse_files(fileNames)
 		return plotWin
+
+
+def SingleQubitRB_AC(qubit, seqFile, showPlot=False):
+	"""
+
+	Single qubit randomized benchmarking using atomic Clifford pulses. 
+
+	Parameters
+	----------
+	qubit : logical channel to implement sequence (LogicalChannel) 
+	seqFile : file containing sequence strings
+	showPlot : whether to plot (boolean)
+
+	Returns
+	-------
+	plotHandle : handle to plot window to prevent destruction
+	"""	
+	#Setup a pulse library
+	pulseLib = [AC(qubit, cliffNum) for cliffNum in range(24)]
+	measBlock = MEAS(qubit)
+
+	with open(seqFile,'r') as FID:
+		fileReader = reader(FID)
+		seqs = []
+		for pulseSeqStr in fileReader:
+			seq = []
+			for pulseStr in pulseSeqStr:
+				seq.append(pulseLib[int(pulseStr)])
+			seq.append(measBlock)
+			seqs.append(seq)
+
+	# #Tack on the calibration scalings
+	# seqs += [[Id(qubit), measBlock], [Id(qubit), measBlock], [X(qubit), measBlock], [X(qubit), measBlock]]
+	# print('Number of sequences: {0}'.format(len(seqs)))
+
+	# fileNames = compile_to_hardware(seqs, 'RB/RB')
+	# print fileNames
+	#Hack for limited APS waveform memory and break it up into multiple files
+	#We've shuffled the sequences so that we loop through each gate length on the inner loop
+	numRandomizations = 32
+	numGateLengths = 9
+	for ct in range(numRandomizations//2):
+		chunk = seqs[2*numGateLengths*ct:2*numGateLengths*(ct+1)]
+		#Tack on the calibration scalings
+		chunk += [[Id(qubit), measBlock], [Id(qubit), measBlock], [X(qubit), measBlock], [X(qubit), measBlock]]
+		print('Number of sequences: {0}'.format(len(chunk)))
+		fileNames = compile_to_hardware(chunk, 'RB/RB', suffix='_{0}'.format(ct+1))
+
+	if showPlot:
+		plotWin = plot_pulse_files(fileNames)
+		return plotWin
+
+
--- PulsePrimitives.py
+++ PulsePrimitives.py
@@ -1,7 +1,8 @@
 import PulseShapes
 import Channels
 
-from scipy.constants import pi
+from math import pi, sin, cos, acos, sqrt
+import numpy as np
 from PulseSequencer import Pulse
 from functools import wraps
 
@@ -108,6 +109,152 @@ def Ym(qubit):
 def Y90m(qubit):
     shape = qubit.pulseParams['shapeFun'](amp=qubit.pulseParams['pi2Amp'], **overrideDefaults(qubit, {}))
     return Pulse("Y90m", qubit, shape, -pi/2, 0.0)
+
+def arb_axis_drag(qubit, nutFreq, rotAngle=0, polarAngle=0, aziAngle=0, **kwargs):
+    """
+    Single qubit arbitrary axis pulse implemented with phase ramping and frame change.
+    For now we assume gaussian shape. 
+
+    Parameters
+    ----------
+    qubit : logical channel
+    rotAngle : effective rotation rotAngle (radians)
+    polarAngle : polar angle of rotation axis (radians)
+    aziAngle : azimuthal (radians)
+    """
+    params = overrideDefaults(qubit, kwargs)
+
+    if params['length'] > 0:
+        #Start from a gaussian shaped pulse
+        gaussPulse = PulseShapes.gaussian(amp=1, **params)
+
+        #To calculate the phase ramping we'll need the sampling rate
+        sampRate = qubit.physicalChannel.samplingRate
+
+        #Scale to achieve to the desired rotation
+        calScale = (rotAngle/2/pi)*sampRate/sum(gaussPulse)
+
+        #Calculate the phase ramp steps to achieve the desired Z component to the rotation axis
+        phaseSteps = -2*pi*cos(polarAngle)*calScale*gaussPulse/sampRate
+
+        #Calculate Z DRAG correction to phase steps
+        #beta is a conversion between XY drag scaling and Z drag scaling
+        beta = params['dragScaling']/sampRate
+        instantaneousDetuning = beta*(2*pi*calScale*sin(polarAngle)*gaussPulse)**2
+        phaseSteps = phaseSteps + instantaneousDetuning*(1.0/sampRate)
+        #center phase ramp around the middle of the pulse time stes
+        phaseRamp = np.cumsum(phaseSteps) - phaseSteps/2
+
+        frameChange = sum(phaseSteps);
+
+        shape = (1.0/nutFreq)*sin(polarAngle)*calScale*np.exp(1j*aziAngle)*gaussPulse
+
+    elif abs(polarAngle) < 1e-10:
+        #Otherwise assuem we have a zero-length Z rotation 
+        frameChange = -rotAngle;
+        shape = np.array([], dtype=np.complex128)
+    else:
+        raise ValueError('Non-zero transverse rotation with zero-length pulse.')
+    
+    return Pulse("ArbAxis", qubit, shape, 0.0, frameChange)
+
+def AC(qubit, cliffNum):
+    """
+
+    The set of 24 Atomic Clifford single qubit pulses.
+
+    Parameters
+    ----------
+    qubit : logical channel to implement sequence (LogicalChannel) 
+    cliffNum : the zero-indexed Clifford number 
+
+    Returns
+    -------
+    pulse object
+    """
+
+    #Figure out the approximate nutation frequency calibration from the X180 and the samplingRate
+    Xp = X(qubit)
+    xpulse = Xp.shape
+    nutFreq = 0.5/(sum(xpulse)/qubit.physicalChannel.samplingRate);
+
+
+    #Now a big else if chain for to get the specific Clifford
+    if cliffNum == 0:
+        #Identity gate
+        return Id(qubit, length=0)
+    elif cliffNum == 1:
+        #X90
+        return X90(qubit)
+    elif cliffNum == 2:
+        #X180
+        return X(qubit)
+    elif cliffNum == 3:
+        #X90m
+        return X90m(qubit)
+    elif cliffNum == 4:
+        #Y90
+        return Y90(qubit)
+    elif cliffNum == 5:
+        #Y180
+        return Y(qubit)
+    elif cliffNum == 6:
+        #Y90m
+        return Y90m(qubit)
+    elif cliffNum == 7:
+        #Z90
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi/2, length=0)
+    elif cliffNum == 8:
+        #Z180
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi, length=0)
+    elif cliffNum == 9:
+        #Z90m
+        return arb_axis_drag(qubit, nutFreq, rotAngle=-pi/2, length=0)
+    elif cliffNum == 10:
+        #X+Y 180
+        return U180(qubit, phase=pi/4)
+    elif cliffNum == 11:
+        #X-Y 180
+        return U180(qubit, phase=-pi/4)
+    elif cliffNum == 12:
+        #X+Z 180(Hadamard)
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi, polarAngle=pi/4)
+    elif cliffNum == 13:
+        #X-Z 180
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi, polarAngle=3*pi/4)
+    elif cliffNum == 14:
+        #Y+Z 180
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi, polarAngle=pi/4, aziAngle=pi/2)
+    elif cliffNum == 15:
+        #Y-Z 180
+        return arb_axis_drag(qubit, nutFreq, rotAngle=pi, polarAngle=3*pi/4, aziAngle=pi/2)
+    elif cliffNum == 16:
+        #X+Y+Z 120
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=acos(1/sqrt(3)), aziAngle=pi/4)
+    elif cliffNum == 17:
+        #X+Y+Z -120 (equivalent to -X-Y-Z 120)
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=pi-acos(1/sqrt(3)), aziAngle=5*pi/4)
+    elif cliffNum == 18:
+        #X-Y+Z 120
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=acos(1/sqrt(3)), aziAngle=-pi/4)
+    elif cliffNum == 19:
+        #X-Y+Z 120 (equivalent ot -X+Y-Z 120)
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=pi-acos(1/sqrt(3)), aziAngle=3*pi/4)
+    elif cliffNum == 20:
+        #X+Y-Z 120
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=pi-acos(1/sqrt(3)), aziAngle=pi/4)
+    elif cliffNum == 21:
+        #X+Y-Z -120 (equivalent to -X-Y+Z 120)
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=acos(1/sqrt(3)), aziAngle=5*pi/4)
+    elif cliffNum == 22:
+        #-X+Y-Z -120 
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=acos(1/sqrt(3)), aziAngle=3**pi/4)
+    elif cliffNum == 23:
+        #-X+Y-Z -120 (equivalent to X-Y-Z 120)
+        return arb_axis_drag(qubit, nutFreq, rotAngle=2*pi/3, polarAngle=pi-acos(1/sqrt(3)), aziAngle=-pi/4)
+    else:
+        raise ValueError('Clifford number must be between 0 and 23')
+
 
 ## two-qubit primitivies
 # @_memoize
