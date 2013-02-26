2b3e165a5dbf34d8ed94eda1453b20099d88618f
Colm Ryan <cryan@bbn.com>
2013-02-26 17:26:40 -0500

    Add a helper function to create calibration sequences.

--- BasicSequences/__init__.py
+++ BasicSequences/__init__.py
@@ -3,3 +3,18 @@ from Ramsey import Ramsey
 from FlipFlop import FlipFlop
 from SPAM import SPAM
 from RB import SingleQubitRB, SingleQubitRB_AC, SingleQubitRBT
+
+
+
+
+from itertools import product
+import operator
+from ..PulsePrimitives import Id, X
+
+def create_cal_seqs(qubits, numCals):
+	"""
+	Helper function to create a set of calibration sequences.
+	"""
+	calSet = [Id, X]
+	calSeqs = [reduce(operator.mul, [p(q) for p,q in zip(pulseSet, qubits)]) for pulseSet in product(calSet, repeat=len(qubits))]
+	return reduce(operator.add, [[[seq]]*numCals for seq in calSeqs])
