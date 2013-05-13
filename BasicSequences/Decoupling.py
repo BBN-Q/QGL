a034577147a8c00ac9ba8b4c3184dce6230a06cd
Colm Ryan <colm@colmryan.org>
2013-05-13 14:39:56 -0400

    Add CPMG sequence to Decopuling module.

--- BasicSequences/Decoupling.py
+++ BasicSequences/Decoupling.py
@@ -1,8 +1,9 @@
 from ..PulsePrimitives import *
 from ..Compiler import compile_to_hardware
 from ..PulseSequencePlotter import plot_pulse_files
+from helpers import create_cal_seqs
 
-def HahnEcho(qubit, pulseSpacings, showPlot=False):
+def HahnEcho(qubit, pulseSpacings, calRepeats=2, showPlot=False):
 	"""
 	A single pulse Hahn echo. 
 
@@ -10,6 +11,7 @@ def HahnEcho(qubit, pulseSpacings, showPlot=False):
 	----------
 	qubit : logical channel to implement sequence (LogicalChannel) 
 	pulseSpacings : pulse spacings to sweep over; the t in 90-t-180-t-180 (iterable)
+	calRepeats : how many times to repeat calibration scalings (default 2)
 	showPlot : whether to plot (boolean)
 
 	Returns
@@ -19,7 +21,42 @@ def HahnEcho(qubit, pulseSpacings, showPlot=False):
 	seqs = [ [X90(qubit), Id(qubit, t), X(qubit), Id(qubit,t), X90(qubit) ,MEAS(qubit)] 
 					 for t in pulseSpacings]
 
+ 	#Tack on the calibration scalings
+	seqs += create_cal_seqs((qubit,), calRepeats)
+
 	fileNames = compile_to_hardware(seqs, 'Echo/Echo')
+	print(fileNames)
+
+	if showPlot:
+		plotWin = plot_pulse_files(fileNames)
+		return plotWin
+
+
+def CPMG(qubit, numPulses, pulseSpacing, calRepeats=2, showPlot=False):
+	"""
+	CPMG pulse train with fixed pulse spacing. 
+
+	Parameters
+	----------
+	qubit : logical channel to implement sequence (LogicalChannel) 
+	numPulses : number of 180 pulses; should be even (iterable)
+	pulseSpacing : spacing between the 180's (seconds)
+	calRepeats : how many times to repeat calibration scalings (default 2)
+	showPlot : whether to plot (boolean)
+
+	Returns
+	-------
+	plotHandle : handle to plot window to prevent destruction
+	"""
+	#First setup the t-180-t block
+	CPMGBlock = [Id(qubit, pulseSpacing/2), Y(qubit), Id(qubit, pulseSpacing/2)]
+
+	seqs = [[X90(qubit)] + CPMGBlock*rep + [X90(qubit)] for rep in numPulses]
+
+ 	#Tack on the calibration scalings
+	seqs += create_cal_seqs((qubit,), calRepeats)
+
+	fileNames = compile_to_hardware(seqs, 'CPMG/CPMG')
 	print(fileNames)
 
 	if showPlot:
--- BasicSequences/__init__.py
+++ BasicSequences/__init__.py
@@ -3,5 +3,5 @@ from T1T2 import Ramsey, InversionRecovery
 from FlipFlop import FlipFlop
 from SPAM import SPAM
 from RB import SingleQubitRB, SingleQubitRB_AC, SingleQubitRBT
-from Decoupling import HahnEcho
+from Decoupling import HahnEcho, CPMG
 from helpers import create_cal_seqs
