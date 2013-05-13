d41d4dbeb621a3150e1a18b21244f71651b773f4
Colm Ryan <colm@colmryan.org>
2013-05-12 20:39:11 -0400

    Add RabiWidth sequence.

--- BasicSequences/Rabi.py
+++ BasicSequences/Rabi.py
@@ -1,8 +1,10 @@
 from ..PulsePrimitives import *
 from ..Compiler import compile_to_hardware
 from ..PulseSequencePlotter import plot_pulse_files
+import QGL.PulseShapes
 
-def RabiAmp(qubit, amps, showPlot=False, phase=0):
+
+def RabiAmp(qubit, amps, phase=0, showPlot=False):
 	"""
 	
 	Variable amplitude Rabi nutation experiment.
@@ -11,8 +13,8 @@ def RabiAmp(qubit, amps, showPlot=False, phase=0):
 	----------
 	qubit : logical channel to implement sequence (LogicalChannel) 
 	amps : pulse amplitudes to sweep over (iterable)
-	showPlot : whether to plot (boolean)
 	phase : phase of the pulse (radians)
+	showPlot : whether to plot (boolean)
 
 	Returns
 	-------
@@ -26,5 +28,32 @@ def RabiAmp(qubit, amps, showPlot=False, phase=0):
 	if showPlot:
 		plotWin = plot_pulse_files(fileNames)
 		return plotWin
+
+def RabiWidth(qubit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh, showPlot=False):
+	"""
+
+	Variable pulse width Rabi nutation experiment.
+
+	Parameters
+	----------
+	qubit : logical channel to implement sequence (LogicalChannel) 
+	widths : pulse widths to sweep over (iterable)
+	phase : phase of the pulse (radians, default = 0)
+	shapeFun : shape of pulse (function, default = PulseShapes.tanh)
+	showPlot : whether to plot (boolean)
+
+	Returns
+	-------
+	plotHandle : handle to plot window to prevent destruction
+	"""
+	seqs = [[Utheta(qubit, length=l, amp=amp, phase=phase, shapeFun=shapeFun), MEAS(qubit)] for l in widths]
+
+	fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+	print(fileNames)
+
+	if showPlot:
+		plotWin = plot_pulse_files(fileNames)
+		return plotWin
+
 
 
