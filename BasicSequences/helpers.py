36097fc0e511e57c6c007a7e8658e4a3a108dcb9
Colm Ryan <colm@colmryan.org>
2013-12-06 12:18:47 -0500

    Same ability to specify measurement channels for create_cal_seqs

--- BasicSequences/helpers.py
+++ BasicSequences/helpers.py
@@ -2,7 +2,7 @@ from itertools import product
 import operator
 from ..PulsePrimitives import Id, X, MEAS
 
-def create_cal_seqs(qubits, numRepeats):
+def create_cal_seqs(qubits, numRepeats, measChans=None):
 	"""
 	Helper function to create a set of calibration sequences.
 
@@ -11,7 +11,11 @@ def create_cal_seqs(qubits, numRepeats):
 	qubits : logical channels, e.g. (q1,) or (q1,q2) (tuple) 
 	numRepeats = number of times to repeat calibration sequences (int)
 	"""
+	if measChans is None:
+		measChans = qubits
+
 	calSet = [Id, X]
 	calSeqs = [reduce(operator.mul, [p(q) for p,q in zip(pulseSet, qubits)]) for pulseSet in product(calSet, repeat=len(qubits))]
+
 	#Add on the measurement operator and repeat
-	return reduce(operator.add, [[[seq]+[MEAS(*qubits)]]*numRepeats for seq in calSeqs])
+	return reduce(operator.add, [[[seq]+[MEAS(*measChans)]]*numRepeats for seq in calSeqs])
