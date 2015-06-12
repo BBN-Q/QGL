206efd6df43c142acbc13340328fd108a1921a4f
Brian Donovan <bdonovan@bbn.com>
2015-06-12 13:26:50 -0400

    Exposed RabiAmpPi function

--- BasicSequences/__init__.py
+++ BasicSequences/__init__.py
@@ -1,4 +1,4 @@
-from Rabi import RabiAmp, RabiWidth, RabiAmp_TwoQubits, PulsedSpec, SingleShot
+from Rabi import RabiAmp, RabiWidth, RabiAmpPi, RabiAmp_TwoQubits, PulsedSpec, SingleShot
 from T1T2 import Ramsey, InversionRecovery
 from FlipFlop import FlipFlop
 from SPAM import SPAM
