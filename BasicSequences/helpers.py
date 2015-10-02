219e908c7c1cfbff50cbe51995efc6c0ab80b814
CoherenceController@Draco <qlab@bbn.com>
2015-10-02 15:19:23 -0400

    Add optional qwait('CMP') to cal seqs
    Necessary when a seq contains branching in order to synchronize all
    decisions
    --DR

--- BasicSequences/helpers.py
+++ BasicSequences/helpers.py
@@ -1,8 +1,9 @@
 from itertools import product
 import operator
 from ..PulsePrimitives import Id, X, MEAS
+from ..ControlFlow import qwait
 
-def create_cal_seqs(qubits, numRepeats, measChans=None):
+def create_cal_seqs(qubits, numRepeats, measChans=None, waitcmp=False):
 	"""
 	Helper function to create a set of calibration sequences.
 
@@ -10,6 +11,7 @@ def create_cal_seqs(qubits, numRepeats, measChans=None):
 	----------
 	qubits : logical channels, e.g. (q1,) or (q1,q2) (tuple) 
 	numRepeats = number of times to repeat calibration sequences (int)
+	waitcmp = True if the sequence contains branching
 	"""
 	if measChans is None:
 		measChans = qubits
@@ -18,5 +20,5 @@ def create_cal_seqs(qubits, numRepeats, measChans=None):
 	#Make all combination for qubit calibration states for n qubits and repeat 
 	calSeqs = [reduce(operator.mul, [p(q) for p,q in zip(pulseSet, qubits)]) for pulseSet in product(calSet, repeat=len(qubits)) for _ in range(numRepeats)]
 
-	#Add on the measurement operator
-	return [[seq, MEAS(*measChans)] for seq in calSeqs]
+	#Add on the measurement operator. 
+	return [[seq, MEAS(*measChans), qwait('CMP')] for seq in calSeqs] if waitcmp else [[seq, MEAS(*measChans)] for seq in calSeqs]
