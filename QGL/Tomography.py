569cd9a7efc6e338b0d8d66867b8c00f2f155ae7
dieris <driste@bbn.com>
2016-02-25 16:04:08 -0500

    Unique tomo pulses
    Prevent the generation of multiple pulses with the same id.
    Specifically, the same slave trigger was referenced to n times, with n
    the number of segments in the sequence. This was causing the Id appended
    to the (same) trigger to grow at every segment. (lingering from PyQlab)

--- QGL/Tomography.py
+++ QGL/Tomography.py
@@ -70,7 +70,14 @@ def process_tomo(seq, qubits, numPulses=4, measChans=None):
 	'''
 	if measChans is None:
 		measChans = qubits
+
+	if len(qubits)==1:
+		return [[prepBlock] + seq + [readoutBlock,  MEAS(*measChans)]
+				for prepBlock, readoutBlock in product(create_tomo_blocks(qubits, numPulses), repeat=2)]
+
+	else:
 	
-	return [[prepBlock] + seq + [readoutBlock,  MEAS(*measChans)]
-				 for prepBlock, readoutBlock in product(create_tomo_blocks(qubits, numPulses), repeat=2)]
+		return [[prepBlock] + seq + [readoutBlock,  MEAS(*measChans)]
+				for prepBlock, readoutBlock in  zip([create_tomo_blocks(qubits,numPulses)[int(k)] for k in (np.floor(np.arange(numPulses**4)/numPulses**2))],
+         		[x for tomo_blocks in [create_tomo_blocks(qubits,numPulses) for _ in range(numPulses**2)] for x in tomo_blocks])]
 
