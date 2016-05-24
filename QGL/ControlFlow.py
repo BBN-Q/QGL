58fdc94246f54b50651e9c975bd0662b64de67d8
Colm Ryan <cryan@bbn.com>
2016-05-23 20:34:27 -0400

    Remove deprecated Pulse.repeat usage

--- QGL/ControlFlow.py
+++ QGL/ControlFlow.py
@@ -47,8 +47,7 @@ def qfunction_specialization(target):
 
 @multimethod(int, Pulse)
 def repeat(n, p):
-    p.repeat = round(n)
-    return p
+	return repeat(n, [Pulse])
 
 @multimethod(int, list)
 def repeat(n, seq):
