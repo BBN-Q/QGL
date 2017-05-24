05ef86cb1ad5eb66df1166edf74ec9735bddff18
Blake R. Johnson <blake.johnson@raytheon.com>
2017-05-24 17:34:30 -0400

    Fix scheduler test to preserve labels in branches.

--- tests/test_Scheduler.py
+++ tests/test_Scheduler.py
@@ -79,15 +79,23 @@ class SchedulerTest(unittest.TestCase):
         # in tensor products, i.e. we want qif(..) * qif(...) to be valid
         q1, q2, q3, q4 = self.q1, self.q2, self.q3, self.q4
 
+        cond_seq = qif(1, [Y(q1),Y(q2),X90(q1)], [Z(q1),Z(q2),X90(q1)])
         seq = [X(q1),X(q2),
                CNOT(q1,q2)] + \
-              qif(1, [Y(q1),Y(q2),X90(q1)], [Z(q1),Z(q2),X90(q1)]) + \
+              cond_seq + \
               [Y90(q1),Y90(q2)]
 
         result = schedule(seq)
+
+        # construct the scheduled conditional from the original to preserve labels
+        cond_seq_scheduled = cond_seq[:2] + \
+                             [Z(q1) * Z(q2), X90(q1)] + \
+                             cond_seq[5:7] + \
+                             [Y(q1) * Y(q2), X90(q1)] + \
+                             cond_seq[-1:]
         assert(result == [X(q1) * X(q2),
                           CNOT(q1, q2)] + \
-                         qif(1, [Y(q1)*Y(q2), X90(q1)], [Z(q1)*Z(q2), X90(q1)]) + \
+                         cond_seq_scheduled + \
                          [Y90(q1) * Y90(q2)])
 
         # test that "global" control flow injects barriers
@@ -97,5 +105,5 @@ class SchedulerTest(unittest.TestCase):
 
         result = schedule(seq)
         assert(result == [X(q1)] + \
-                         qif(1, [Y(q1)*Y(q2), X90(q1)], [Z(q1)*Z(q2), X90(q1)]) + \
+                         cond_seq_scheduled + \
                          [Y90(q1) * Y90(q2)])
