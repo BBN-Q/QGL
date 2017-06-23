e50eba650833b5dafeef1ebeb1419e4bdbbad7db
Graham Rowlands <grahamrow@gmail.com>
2017-06-23 13:24:32 -0400

    Beginnings of loading from YAML

--- QGL/ChannelLibrary.py
+++ QGL/ChannelLibrary.py
@@ -19,19 +19,67 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+Include modification to yaml loader (MIT License) from 
+https://gist.github.com/joshbode/569627ced3076931b02f
+
+Scientific notation fix for yaml from
+https://stackoverflow.com/questions/30458977/yaml-loads-5e-6-as-string-and-not-a-number
 '''
 
 import sys
+import os
+import re
 import json
 import importlib
 from atom.api import Atom, Str, Int, Typed
 import networkx as nx
+import yaml
 
 from . import Channels
 from . import PulseShapes
 from JSONLibraryUtils import LibraryCoders, FileWatcher, JSONMigrators
 from . import config
 
+class LoaderMeta(type):
+    def __new__(metacls, __name__, __bases__, __dict__):
+        """Add include constructer to class."""
+        # register the include constructor on the class
+        cls = super().__new__(metacls, __name__, __bases__, __dict__)
+        cls.add_constructor('!include', cls.construct_include)
+        return cls
+class Loader(yaml.Loader, metaclass=LoaderMeta):
+    """YAML Loader with `!include` constructor."""
+    def __init__(self, stream):
+        """Initialise Loader."""
+        try:
+            self._root = os.path.split(stream.name)[0]
+        except AttributeError:
+            self._root = os.path.curdir
+        super().__init__(stream)
+        self.add_implicit_resolver(
+            u'tag:yaml.org,2002:float',
+            re.compile(u'''^(?:
+             [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+            |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+            |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+            |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+            |[-+]?\\.(?:inf|Inf|INF)
+            |\\.(?:nan|NaN|NAN))$''', re.X),
+            list(u'-+0123456789.'))
+        self.filenames = [os.path.abspath(stream.name)]
+    def construct_include(self, node):
+        """Include file referenced at node."""
+        filename = os.path.abspath(os.path.join(
+            self._root, self.construct_scalar(node)
+        ))
+        extension = os.path.splitext(filename)[1].lstrip('.')
+        self.filenames.append(filename)
+        with open(filename, 'r') as f:
+            if extension in ('yaml', 'yml'):
+                return yaml.load(f, Loader)
+            else:
+                return ''.join(f.readlines())
 
 class ChannelLibrary(Atom):
     # channelDict = Dict(Str, Channel)
@@ -48,9 +96,9 @@ class ChannelLibrary(Atom):
         super(ChannelLibrary, self).__init__(channelDict=channelDict, **kwargs)
         self.connectivityG = nx.DiGraph()
         self.load_from_library()
-        if self.libFile:
-            self.fileWatcher = FileWatcher.LibraryFileWatcher(
-                self.libFile, self.update_from_file)
+        # if self.libFile:
+        #     self.fileWatcher = FileWatcher.LibraryFileWatcher(
+        #         self.libFile, self.update_from_file)
 
     #Dictionary methods
     def __getitem__(self, key):
@@ -102,25 +150,127 @@ class ChannelLibrary(Atom):
         return {"channelDict": self.channelDict, "version": self.version}
 
     def load_from_library(self):
+        """Loads the YAML library, creates the QGL objects, and returns a list of the visited filenames 
+        for the filewatcher."""
         if not self.libFile:
             return
         try:
             with open(self.libFile, 'r') as FID:
-                tmpLib = json.load(FID, cls=ChannelDecoder)
-            if not isinstance(tmpLib, ChannelLibrary):
-                raise ValueError('Failed to load channel library')
+                loader = Loader(FID)
+                try:
+                    tmpLib = loader.get_single_data()
+                    filenames = loader.filenames
+                finally:
+                    loader.dispose()
 
+            instr_dict  = {instr["name"]: instr for instr in tmpLib['instruments']}
+            qubit_dict  = {qubit["name"]: qubit for qubit in tmpLib['qubits']}
+            filter_dict = {filt["name"]: filt for filt in tmpLib['filters']}
+            master_awg  = []
+
+            # Construct the channel library
+            channel_dict = {}
+
+            for instr in instr_dict.values():
+                if "channels" in instr.keys():
+                    for channel in instr["channels"]:
+                        params = {k: v for k,v in channel.items() if k in Channels.PhysicalQuadratureChannel.__atom_members__.keys()}
+                        params["label"] = instr["name"] + "-" + channel["name"]
+                        params["instrument"] = instr["name"]
+                        params["translator"] = instr["type"] + "Pattern"
+                        params["__module__"] = "QGL.Channels"
+                        params["__class__"]  = "PhysicalQuadratureChannel"
+                        channel_dict[params["label"]] = params
+                if "markers" in instr.keys():
+                    for marker in instr["markers"]:
+                        params = {k: v for k,v in marker.items() if k in Channels.PhysicalMarkerChannel.__atom_members__.keys()}
+                        params["label"] = instr["name"] + "-" + marker["name"]
+                        params["instrument"] = instr["name"]
+                        params["__module__"] = "QGL.Channels"
+                        params["__class__"]  = "PhysicaMarkerChannel"
+                        channel_dict[params["label"]] = params
+                if "master" in instr.keys():
+                    if instr["master"]:
+                        master_awg.append(params["label"])
+
+            # Establish the slave trigger
+            if len(master_awg) != 1:
+                raise ValueError("More (or less) than one AWG is marked as master.")
+            else:
+                params = {}
+                params["label"]       = "slaveTrig"
+                params["physChan"]    = master_awg[0] + "-m1"
+                params["pulseParams"] = {"length": 1e-7, "shapeFun": "constant"}
+                params["__module__"]  = "QGL.Channels"
+                params["__class__"]   = "LogicalMarkerChannel"
+                channel_dict[params["label"]] = params
+
+            for filt in filter_dict.values():
+                if "StreamSelector" in filt["type"]:
+                    params = {k: v for k,v in marker.items() if k in Channels.ReceiverChannel.__atom_members__.keys()}
+                    params["label"]      = instr_dict[filt["instrument"]]["name"] + "-" + filt["name"]
+                    params["__module__"] = "QGL.Channels"
+                    params["__class__"]  = "ReceiverChannel"
+                    channel_dict[params["label"]] = params
+
+            for qubit in qubit_dict.values():
+                # Create the Qubits
+                params = {k: v for k,v in qubit["control"].items() if k in Channels.Qubit.__atom_members__.keys()}
+                params["label"] = "{}".format(qubit["name"])
+                params["__module__"] = "QGL.Channels"
+                params["__class__"]  = "Qubit"
+                channel_dict[params["label"]] = params
+
+                # Create the measurements
+                params = {k: v for k,v in qubit["measure"].items() if k in Channels.Measurement.__atom_members__.keys()}
+                params["label"]      = "M-{}".format(qubit["name"])
+                params["__module__"] = "QGL.Channels"
+                params["__class__"]  = "Measurement"
+                channel_dict[params["label"]] = params
+
+                # Create the receiver channels
+                instr, chan, stream = qubit["measure"]["receiver"].split()
+                phys_instr, phys_marker = qubit["measure"]["trigger"].split()
+                params = {k: v for k,v in marker.items() if k in Channels.LogicalMarkerChannel.__atom_members__.keys()}
+                params["label"]      = "digTrig-" + instr
+                params["physChan"]   = phys_instr + "-" + phys_marker
+                params["__module__"] = "QGL.Channels"
+                params["__class__"]  = "LogicalMarkerChannel"
+                # Don't duplicate triggers to the same digitizer
+                if (params["label"]) not in channel_dict.keys():
+                    channel_dict[params["label"]] = params
+
+                # Create the measurement gate chan:
+                instr, chan = qubit["measure"]["gate"].split()
+                phys_instr, phys_marker = qubit["measure"]["gate"].split()
+                params["label"]      = "M-{}-gate".format(qubit["name"])
+                params["physChan"]   = phys_instr + "-" + phys_marker
+                params["__module__"] = "QGL.Channels"
+                params["__class__"]  = "LogicalMarkerChannel"
+                channel_dict[params["label"]] = params
+
+                # Create the control gate chan:
+                instr, chan = qubit["control"]["gate"].split()
+                phys_instr, phys_marker = qubit["control"]["gate"].split()
+                params["label"]      = "{}-gate".format(qubit["name"])
+                params["physChan"]   = phys_instr + "-" + phys_marker
+                params["__module__"] = "QGL.Channels"
+                params["__class__"]  = "LogicalMarkerChannel"
+                channel_dict[params["label"]] = params
+
+            
             # connect objects labeled by strings
-            for chan in tmpLib.channelDict.values():
+            for chan in channel_dict.values():
                 for param in self.specialParams:
                     if hasattr(chan, param):
                         setattr(chan, param,
-                                tmpLib.channelDict.get(getattr(chan, param), None)
+                                channel_dict.get(getattr(chan, param), None)
                                 )
-            self.channelDict.update(tmpLib.channelDict)
+            self.channelDict.update(channel_dict)
             # grab library version
-            self.version = tmpLib.version
+            # self.version = tmpLib.version
             self.build_connectivity_graph()
+            return filenames
         except IOError:
             print('No channel library found.')
         except ValueError:
@@ -223,8 +373,8 @@ class ChannelDecoder(json.JSONDecoder):
 
             __import__(moduleName)
 
-            #Re-encode the strings as ascii (this should go away in Python 3)
-            jsonDict = {str(k): v for k, v in jsonDict.items()}
+            # #Re-encode the strings as ascii (this should go away in Python 3)
+            # jsonDict = {str(k): v for k, v in jsonDict.items()}
 
             # instantiate the object
             inst = getattr(sys.modules[moduleName], className)(**jsonDict)
@@ -232,7 +382,7 @@ class ChannelDecoder(json.JSONDecoder):
             return inst
         else:
             #Re-encode the strings as ascii (this should go away in Python 3)
-            jsonDict = {str(k): v for k, v in jsonDict.items()}
+            # jsonDict = {str(k): v for k, v in jsonDict.items()}
             shapeFun = jsonDict.pop('shapeFun', None)
             if shapeFun:
                 jsonDict['shapeFun'] = getattr(QGL.PulseShapes, shapeFun)
@@ -269,9 +419,9 @@ def EdgeFactory(source, target):
             source, target)))
 
 # global channel library
-migrator = JSONMigrators.ChannelMigrator(config.channelLibFile)
-migrationMsg = migrator.migrate()
-for msg in migrationMsg:
-    print(msg)
+# migrator = JSONMigrators.ChannelMigrator(config.channelLibFile)
+# migrationMsg = migrator.migrate()
+# for msg in migrationMsg:
+    # print(msg)
 
-channelLib = ChannelLibrary(libFile=config.channelLibFile)
+channelLib = ChannelLibrary(libFile=config.configFile)
