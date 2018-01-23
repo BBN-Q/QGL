9f89dd65d69ba48855ef0255e145f1ad2069283f
Daniel Ellard <ellard@bbn.com>
2018-01-23 11:39:08 -0500

    cleanup and complete (save output...)

--- QGL/n.py
+++ QGL/n.py
@@ -1,21 +1,25 @@
+# Example program for creating TDM instruction files
+
+import copy
+import json
+import numpy
+import os
+import shutil
 
 import QGL.drivers
 from QGL import *
-import json
-import numpy as np
-import pprint
-import time
 import aps2_reader
 
 from QGL.drivers.APS2TDMPattern import Instruction
 import QGL.drivers.APS2TDMPattern
 
-# import aps2
-
-ChannelLibrary(blank=True)
-
-_q1 = Qubit(label='q1')
-_q2 = Qubit(label='q2')
+def pp_instructions(name, instructions):
+    print('')
+    print('INSTRUMENT: ' + name)
+    for i in range(len(instructions)):
+        instr_bits = instructions[i]
+        instr_txt = str(Instruction.unflatten(instr_bits))
+        print('%5d: 0x%.16x - %s' % (i, instr_bits, instr_txt))
 
 def setUp():
     # Copied from the unittests (CompileUtils).
@@ -51,8 +55,8 @@ def setUp():
     """
     ChannelLibraries.channelLib.build_connectivity_graph()
 
+ChannelLibrary(blank=True)
 setUp()
-
 
 q1 = QubitFactory('q1')
 q2 = QubitFactory('q2')
@@ -68,39 +72,50 @@ seq = [
         MEASA(q1, maddr=(10, 0)),
         # MEASA(q1, maddr=(10, 1)),
         # MEASA(q1, maddr=(10, 2)),
-        MEASA(q1, maddr=(20, 0)),
+        # MEASA(q2, maddr=(20, 0)),
 
-        LoadCmpTdm(0xfedc, 0x1234678),
+        # LoadCmpTdm(0xfedc, 0x1234678),
 
         MajorityVote(10, 11),
         ]
 
-aps_metafile = compile_to_hardware([seq], '/tmp/f')
-tdm_instr = QGL.drivers.APS2TDMPattern.get_tdm_instructions()
+# First, compile for the APS units.  As a side effect,
+# this creates the TDM instructions, but does NOT
+# put them into the APS output file.  We retrieve
+# the TDM instructions, and then recompile the instructions
+# to create a template TDM file, and then insert the
+# TDM instructions into the template.
+#
+# Note that the steps of compiling for the TDM and
+# inserting the instructions into the file are FRAGILE
+# because they require that there be an instrument named
+# "BBNAPS1" in the machine config.  This name has special
+# meaning.
 
+# IMPORTANT: compilation is destructive: it modifies
+# the input sequences (and possibly the instances in those
+# sequences.  So, running compiler_to_hardware on the
+# same sequence twice can FAIL (or give weird results).
+# So copy the input sequence each time we use it...
+
+aps_metafile = compile_to_hardware([copy.copy(seq)], '/tmp/aps')
+tdm_instr = QGL.drivers.APS2TDMPattern.get_tdm_instructions()
 aps_metadata = json.loads(open(aps_metafile).read())
-print(aps_metadata)
+
+tdm_metafile = compile_to_hardware([copy.copy(seq)], '/tmp/tdm')
+tdm_metadata = json.loads(open(tdm_metafile).read())
+aps2_reader.replace_instructions(
+        tdm_metadata['instruments']['BBNAPS1'],
+        numpy.array(tdm_instr, dtype=np.uint64))
 
 for key in aps_metadata['instruments']:
-    print('')
-    print('INSTRUMENT %s' % str(key))
-    instructions = aps2_reader.raw_instructions(aps_metadata['instruments'][key])
-    # print('TYPE %s' % str(type(instructions)))
-    # aps2_reader.display_decompiled_instructions(instructions)
+    instructions = aps2_reader.raw_instructions(
+            aps_metadata['instruments'][key])
+    pp_instructions(str(key), instructions)
 
-    for i in range(len(instructions)):
-        instr_bits = instructions[i]
-        instr_txt = str(Instruction.unflatten(instr_bits))
-        print('%5d: 0x%.16x - %s' % (i, instr_bits, instr_txt))
-
-print('')
-print('INSTRUMENT tdm')
-for i in range(len(tdm_instr)):
-    instr_bits = tdm_instr[i]
-    instr_txt = str(Instruction.unflatten(instr_bits))
-    print('%5d: 0x%.16x - %s' % (i, instr_bits, instr_txt))
-
-# TODO: insert TDM instructions into the output file
-
-
+# Read the TDM instructions from file, just to make sure
+#
+tdm_instr_from_file = aps2_reader.raw_instructions(
+        tdm_metadata['instruments']['BBNAPS1'])
+pp_instructions('tdm', tdm_instr_from_file)
 
