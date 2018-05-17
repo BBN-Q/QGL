8f5cf63191888d7180ab57a30068633032766a5c
gribeill <guilhem.ribeill@gmail.com>
2018-05-17 17:07:29 -0400

    Various bugfixes, first pass at Pauli random compilation.

--- QGL/GSTTools.py
+++ QGL/GSTTools.py
@@ -20,8 +20,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 from .PulsePrimitives import *
+from .Cliffords import *
 from pygsti.objects import GateString
 from itertools import chain
+from random import choices
 
 #Default mapping from pyGSTi naming convention to QGL gates.
 gst_gate_map = {"Gx": X,
@@ -41,7 +43,7 @@ def gst_map_1Q(gst_list, qubit, qgl_map=gst_gate_map, append_meas=True):
     Returns:
         QGL sequences, preserving the input list nesting (as a generator)
     """
-    if isinstace(gst_list, GateString):
+    if isinstance(gst_list, GateString):
         gst_list = [gst_list]
     for item in gst_list:
         if isinstance(item, GateString):
@@ -51,9 +53,53 @@ def gst_map_1Q(gst_list, qubit, qgl_map=gst_gate_map, append_meas=True):
             else:
                 yield list(mapped)
         elif isinstance(item, list):
-            yield list(gst_map(item, qubit, qgl_map=qgl_map, append_meas=append_meas))
+            yield list(gst_map_1Q(item, qubit, qgl_map=qgl_map, append_meas=append_meas))
 
 def create_gst_sequence_from_pygsti(gst_list, qubit, gate_map=gst_gate_map):
     """ Returns list of QGL sequences from a pyGSTi GateString list. See gst_map_1Q.
+        The return value is a list of sequences that can be complied by QGL.
     """
-    return list(gst_map_1Q(gst_list, qubit, qgl_map=gate_map, append_meas=True))
+    return list(chain(*gst_map_1Q(gst_list, qubit, qgl_map=gate_map, append_meas=True)))
+
+def pauli_rand_clifford_circuit(gst_seq):
+
+    def seqreduce(s):
+        if not s:
+            return 0
+        else:
+            return reduce(lambda x,y: clifford_multiply(x,y), s)
+
+    def inv_cliff(c):
+        return inverse_clifford(clifford_mat(c, 1))
+
+    c_ps = [0, 2, 5, 8]
+    cliff_map = {"Gi": 0,
+                 "Gx": 2,
+                 "Gy": 5}
+
+    #convert to dictionary of lambdas for compatibility with gst_map_1Q
+    lambda_map = {k: lambda x, v=v: v for k, v in cliff_map.items()}
+
+    c_seqs = list(chain(*gst_map_1Q(gst_seq, None, qgl_map=lambda_map,
+                                    append_meas=False)))
+    r_seqs = []
+    for seq in c_seqs:
+        if not seq:
+            r_seqs.append([])
+        else:
+            rand_pauli = choices(c_ps, k=len(seq))
+            inter = 0
+            bare = 0
+            rseq = []
+            for j in range(len(seq)):
+                inter = clifford_multiply(clifford_multiply(inter, rand_pauli[j]), seq[j])
+                bare = clifford_multiply(bare, seq[j])
+                rseq.append(clifford_multiply(rand_pauli[j], seq[j]))
+            recovery = clifford_multiply(inv_cliff(inter), bare)
+            rseq[-1] = clifford_multiply(rseq[-1], recovery)
+            r_seqs.append(rseq)
+
+    all_ok = all((r == i for r, i in zip(map(seqreduce, r_seqs), map(seqreduce, c_seqs))))
+    assert all_ok, "Something went wrong when Pauli-frame randomizing!"
+
+    return r_seqs, c_seqs
