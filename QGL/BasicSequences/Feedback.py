905c61794e8d0161388f66c6eac965a21a222853
Diego Ristè <diego.riste@raytheon.com>
2018-06-11 09:24:45 -0400

    Example of bit flip code

--- QGL/BasicSequences/Feedback.py
+++ QGL/BasicSequences/Feedback.py
@@ -5,6 +5,7 @@ from .helpers import create_cal_seqs
 from itertools import product
 import operator
 from ..ControlFlow import *
+from ..TdmInstructions import *
 from functools import reduce
 
 
@@ -99,3 +100,58 @@ def Reset(qubits,
     if showPlot:
         plot_pulse_files(metafile)
     return metafile
+
+
+# do not make it a subroutine for now
+def BitFlip3(data_qs, ancilla_qs, theta=None, phi=None, nrounds=1, meas_delay=1e-6):
+    """
+
+    Encoding on 3-qubit bit-flip code, followed by n rounds of syndrome detection, and final correction using the n results.
+
+    Parameters
+    ----------
+    data_qs : tuple of logical channels for the 3 code qubits
+    ancilla_qs: tuple of logical channels for the 2 syndrome qubits
+    theta, phi: longitudinal and azimuthal rotation angles for encoded state (default = no encoding)
+    meas_delay : delay between syndrome check rounds
+    docals, calRepeats: enable calibration sequences, repeated calRepeats times
+
+    Returns
+    -------
+    plotHandle : handle to plot window to prevent destruction
+    """
+    if len(data_qs) != 3 or len(ancilla_qs) != 2:
+        raise Exception("Wrong number of qubits")
+    seqs =  [
+    DecodeSetRounds(1,0,2**nrounds-1),
+    Invalidate(addr=10, mask=2**nrounds-1),
+    Invalidate(addr=11, mask=0x1)]
+
+    # encode single-qubit state into 3 qubits
+    if theta and phi:
+        seqs+=[Utheta(data_qs[1], theta, phi), CNOT(data_qs[1], data_qs[0]), CNOT(data_qs[1], data_qs[2])]
+
+    # multiple rounds of syndrome measurements
+    for k in range(nrounds):
+        seqs+=[CNOT(data_qs[0],ancilla_qs[0])*CNOT(data_qs[1],ancilla_qs[1])],
+        seqs+=[CNOT(data_qs[1], ancilla_qs[0])*CNOT(data_qs[2],ancilla_qs[1])],
+        seqs+= [MEASA(ancilla_qs[0], maddr=(10,2*k+1))*MEASA(ancilla_qs[1], maddr=(10,2*k+2)),
+        Id(ancilla_qs[0], meas_delay)*
+        MEAS(data_qs[0],amp=0)*MEAS(data_qs[1],amp=0)*MEAS(data_qs[2],amp=0)] # virtual msmt's just to keep the number of segments uniform across digitizer channels
+    seqs+=Decode(10, 11, 2**nrounds-1),
+    seqs+=qwait("RAM",11, 1)
+    seqs+=[MEAS(data_qs[0])*MEAS(data_qs[1])*MEAS(data_qs[2])
+    *MEAS(ancilla_qs[0],amp=0)*MEAS(ancilla_qs[1],amp=0)] # virtual msmt's just to keep the number
+
+    # apply corrective pulses depending on the decoder result
+    FbGates = []
+    for q in data_qs:
+        FbGates.append([gate(q) for gate in [Id, X]])
+    FbSeq = [reduce(operator.mul, x) for x in product(*FbGates)]
+    for k in range(8):
+        seqs += qif(k, [FbSeq[k]])
+    if docals:
+        seqs += create_cal_seqs(qubits,
+        calRepeats,
+        measChans=measChans)
+    return seqs
