1bbe3c0a7ea24e3e1d0bade9f7176c4f3469fbbd
Brian Donovan <bdonovan@bbn.com>
2018-11-28 12:52:58 -0500

    Merge conflict

--- QGL/drivers/APS2Pattern.py
+++ QGL/drivers/APS2Pattern.py
@@ -49,6 +49,7 @@ MAX_REPEAT_COUNT = 2**16 - 1
 MAX_TRIGGER_COUNT = 2**32 - 1
 MAX_VRAM_ADDRESS = 2**12-1
 MODULATION_CLOCK = 300e6
+NUM_NCO = 4
 
 # instruction encodings
 WFM = 0x0
@@ -581,13 +582,19 @@ class ModulationCommand(object):
         MODULATOR_OP_OFFSET = 44
         NCO_SELECT_OP_OFFSET = 40
 
+        nco_select_bits = {1 : 0b0001,
+                           2 : 0b0010,
+                           3 : 0b0100,
+                           4 : 0b1000,
+                           15: 0b1111}[self.nco_select]
+
         op_code_map = {"MODULATE": 0x0,
                        "RESET_PHASE": 0x2,
                        "SET_FREQ": 0x6,
                        "SET_PHASE": 0xa,
                        "UPDATE_FRAME": 0xe}
         payload = (op_code_map[self.instruction] << MODULATOR_OP_OFFSET) | (
-            self.nco_select << NCO_SELECT_OP_OFFSET)
+            (nco_select_bits) << NCO_SELECT_OP_OFFSET)
         if self.instruction == "MODULATE":
             #zero-indexed quad count
             payload |= np.uint32(self.length / ADDRESS_UNIT - 1)
@@ -615,9 +622,9 @@ def inject_modulation_cmds(seqs):
     for ct,seq in enumerate(seqs):
         #check whether we have modulation commands
         freqs = np.unique([entry.frequency for entry in filter(lambda s: isinstance(s,Compiler.Waveform), seq)])
-        if len(freqs) > 2:
-            raise Exception("Max 2 frequencies on the same channel allowed.")
-        no_freq_cmds = np.all(np.less(np.abs(freqs), 1e-8))
+        if len(freqs) > NUM_NCO:
+            raise Exception("Max {} frequencies on the same channel allowed.".format(NUM_NCO))
+        no_freq_cmds = np.allclose(freqs, 0)
         phases = [entry.phase for entry in filter(lambda s: isinstance(s,Compiler.Waveform), seq)]
         no_phase_cmds = np.all(np.less(np.abs(phases), 1e-8))
         frame_changes = [entry.frameChange for entry in filter(lambda s: isinstance(s,Compiler.Waveform), seq)]
@@ -1135,12 +1142,13 @@ def read_sequence_file(fileName):
         instructions = np.frombuffer(FID.read(8*inst_len), dtype=np.uint64)
 
         wf_lib = {}
-        for i in range(num_chans):
-            wf_len  = struct.unpack('<Q', FID.read(8))[0]
-            wf_dat  = np.frombuffer(FID.read(2*wf_len), dtype=np.int16)
-            wf_lib[f'ch{i+1}'] = ( 1.0 / MAX_WAVEFORM_VALUE) * wf_dat.flatten()
-
-        NUM_NCO = 2
+        wf_lib['ch1'] = (
+            1.0 /
+            MAX_WAVEFORM_VALUE) * FID['/chan_1/waveforms'].value.flatten()
+        wf_lib['ch2'] = (
+            1.0 /
+            MAX_WAVEFORM_VALUE) * FID['/chan_2/waveforms'].value.flatten()
+        instructions = FID['/chan_1/instructions'].value.flatten()
         freq = np.zeros(NUM_NCO)  #radians per timestep
         phase = np.zeros(NUM_NCO)
         frame = np.zeros(NUM_NCO)
@@ -1267,7 +1275,6 @@ def read_sequence_file(fileName):
                 seqs['ch2'][ct] = mod_ch2
 
         del seqs['mod_phase']
-
     return seqs
 
 
