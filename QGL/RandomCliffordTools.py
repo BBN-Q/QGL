79fffaaec19a68267a4a4c381d27446b8e898d26
Guilhem Ribeill <guilhem.ribeill@gmail.com>
2019-02-27 16:56:09 -0500

    Only set up clifford generator once per sequence file.

--- QGL/RandomCliffordTools.py
+++ QGL/RandomCliffordTools.py
@@ -33,13 +33,15 @@ from . import TdmInstructions # only for APS2-TDM
 from .APS2CustomInstructions import *
 from .Cliffords import C1, inverse_clifford, clifford_multiply
 
+default_clifford_options = {"offset": 0x0, "spacing": 0x1, "seed": 0x31}
+
 VALID_CLIFFORD_TYPES = ('RandomAC',)
 
 def generate_clifford_jump_table(cliff_wires, jt_label = None):
     """Generate the jump table that will be used to call into the clifford set"""
 
     if jt_label is None:
-        jt_label = BlockLabel.BlockLabel("JT")
+        jt_label = BlockLabel.BlockLabel("JT", jump_table=True, table_size=48)
     if not isinstance(jt_label, BlockLabel.BlockLabel):
         raise ValueError("Jump table label must be a BlockLabel.")
 
@@ -55,13 +57,21 @@ def generate_clifford_jump_table(cliff_wires, jt_label = None):
 
     cliff_wires.insert(0, jump_table)
 
+    return jt_label
+
 def insert_clifford_calls(seqs, jt_label=None, cliff_addr=0x3, add_inv = True,
-                            inv_addr=0x3, inv_values=[]):
+                            inv_addr=0x3, inv_values=[],
+                            clifford_options=default_clifford_options):
 
     if jt_label is None:
         jt_label = BlockLabel.BlockLabel("JT")
     if not isinstance(jt_label, BlockLabel.BlockLabel):
         raise ValueError("Jump table label must be a BlockLabel.")
+
+    #Insert defaults
+    for k,v in default_clifford_options.items():
+        if k not in clifford_options.keys():
+            cliford_options[k] = v
 
     for idx, seq in enumerate(seqs[:]):
         if not PatternUtils.contains_runtime_pulses(seq):
@@ -74,18 +84,14 @@ def insert_clifford_calls(seqs, jt_label=None, cliff_addr=0x3, add_inv = True,
                 and pulse.label in VALID_CLIFFORD_TYPES:
                 has_random_cliff = True
                 new_seq.extend(RandomClifford(jt_label, cliff_addr))
-                print("Inserting clifford pulse!")
+                #print("Inserting clifford pulse!")
             else:
                 new_seq.append(pulse)
 
-        info_seqs = []
-        info_seqs.extend(RandomCliffordSetOffset(0x1, 0x0))
-        info_seqs.extend(RandomCliffordSetSpacing(0x2, 0x1))
-
-        if isinstance(seq[0], BlockLabel.BlockLabel):
-            new_seq[1:1] = info_seqs
-        else:
-            new_seq[0:0] = info_seqs
+        # if isinstance(seq[0], BlockLabel.BlockLabel):
+        #     new_seq[1:1] = info_seqs
+        # else:
+        #     new_seq[0:0] = info_seqs
 
         if add_inv:
             #insert reset after first wait
@@ -98,6 +104,12 @@ def insert_clifford_calls(seqs, jt_label=None, cliff_addr=0x3, add_inv = True,
                   new_seq.extend(RandomCliffordInverse(jt_label, inv_addr))
 
         seqs[idx] = new_seq
+
+    info_seqs = []
+    info_seqs.extend(RandomCliffordSetOffset(0x1, clifford_options["offset"]))
+    info_seqs.extend(RandomCliffordSetSpacing(0x2, clifford_options["spacing"]))
+    info_seqs.extend(RandomCliffordSeed(clifford_options["seed"]))
+    seqs.insert(0, info_seqs)
 
 
 def randomize_clifford_sequences(qubit, seqs, clifford_type=PulsePrimitives.RandomAC):
