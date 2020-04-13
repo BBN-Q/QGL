ae7f22b5fc606a8415e286ffabd43d3fbb71977c
Guilhem Ribeill <guilhem.ribeill@gmail.com>
2020-04-13 17:31:54 -0400

    Add Euler angle conversion tests.

---
+++ tests/test_euler.py
@@ -0,0 +1,39 @@
+import unittest
+
+from QGL import *
+from QGL.Euler import *
+from QGL.Cliffords import C1
+import QGL.config
+try:
+  from helpers import setup_test_lib
+except:
+  from .helpers import setup_test_lib
+
+class EulerDecompositions(unittest.TestCase):
+
+	N_test = 1000
+
+	def setUp(self):
+		pass
+		#setup_test_lib()
+		#self.q1 = QubitFactory('q1')
+
+	def test_zyz_decomp(self):
+		for j in range(self.N_test):
+			Uh = haar_unitary(2)
+			Ux = zyz_unitary(*zyz_angles(Uh))
+			assert is_close(Uh, Ux)
+
+	def test_xyx_decomp(self):
+		for j in range(self.N_test):
+			Uh = haar_unitary(2)
+			Ux = xyx_unitary(*xyx_angles(Uh))
+			assert is_close(Uh, Ux)
+
+	def test_xyx_cliffords(self):
+		for j in range(24):
+			Uxyx = xyx_unitary(*xyx_angles(C1[j]))
+			assert is_close(Uxyx, C1[j]), f"{j}"
+
+if __name__ == "__main__":
+	unittest.main()
