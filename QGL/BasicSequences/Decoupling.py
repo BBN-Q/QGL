1e64ab858585fda9147ec3f5f3189ce40657b316
Matthew Ware <matt.ware@raytheon.com>
2021-04-04 11:40:50 -0400

    JAZZ sequence

--- QGL/BasicSequences/Decoupling.py
+++ QGL/BasicSequences/Decoupling.py
@@ -117,3 +117,65 @@ def CPMG(qubit: Channels.LogicalChannel,
     if showPlot:
         plot_pulse_files(metafile)
     return metafile
+
+def JAZZ(qubit: Channels.LogicalChannel,
+         spec_qubit: Channels.LogicalChannel,
+         pulseSpacings: Iterable[Union[int, float]],
+         periods: int = 0, 
+         calRepeats: int = 2, 
+         showPlot: bool = False) -> str:
+    """
+    Implement a JAZZ sequence on a pair of coupled qubits. A Ramsey experiment
+    is run on the first qubit and pi pulses are toggled on the second.
+
+    Parameters
+    ----------
+    qubit :  Channels.LogicalChannel
+        Logical channel to measure
+    spec_qubit :  Channels.LogicalChannel
+        Logical channel to echo
+    pulseSpacings : int iterable
+        Pulse spacings to sweep over; the t in 90-t-180-t-180 (iterable)
+    periods : int, optional
+        Number of artificial oscillations
+    calRepeats : int, optional
+        How many times to repeat calibration scalings (default 2)
+    showPlot : boolean, optional
+        Whether to plot
+
+    Returns
+    -------
+    metafile : string
+        Path to a json metafile with details about the sequences and paths
+        to compiled machine files
+
+    Examples
+    --------
+    >>> mf = JAZZ(q1, q2, np.linspace(20.0e-9, 200.02e-6, 101), periods=6);
+    Compiled 105 sequences.
+    >>> mf
+    '/path/to/exp/exp-meta.json'
+    """
+    seqs = []
+    for k in range(len(pulseSpacings)):
+        seqs.append([X90(qubit), Id(qubit, pulseSpacings[k]), \
+        Y(qubit)*X(spec_qubit), Id(qubit,pulseSpacings[k]), \
+        U90(qubit,phase=2*pi*periods/len(pulseSpacings)*k), MEAS(qubit)])
+
+    for k in range(len(pulseSpacings)):
+        seqs.append([X90(qubit)*X(spec_qubit), Id(qubit, pulseSpacings[k]), \
+        Y(qubit)*X(spec_qubit), Id(qubit,pulseSpacings[k]), \
+        U90(qubit,phase=2*pi*periods/len(pulseSpacings)*k), MEAS(qubit)])
+
+    # Tack on the calibration scalings
+    seqs += create_cal_seqs((qubit, ), calRepeats)
+
+    metafile = compile_to_hardware(seqs, 'JAZZ/JAZZ',
+        axis_descriptor=[
+            delay_descriptor(np.hstack(np.squeeze([2 * pulseSpacings] *2))),
+            cal_descriptor((qubit,), calRepeats)
+        ])
+
+    if showPlot:
+        plot_pulse_files(metafile)
+    return metafile
